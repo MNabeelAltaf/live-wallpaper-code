1dc4095e6bb4b8d435d0303836c8a0c1bdedbbeb
Maya719 <shoaib.mobipixels@gmail.com>
2024-12-05 12:44:20 +0500

    search api

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -11,26 +11,19 @@ class Categories extends Model
 
     protected $fillable = ['name', 'thumbnail', 'show', 'type'];
     protected $table = 'categories';
-
-    // Relation to StaticWallpapers: A category has many static wallpapers
     public function wallpapers()
     {
         return $this->hasMany(StaticWallpaper::class, 'cat_id');
     }
-
-    // Relation to FourDwallpapers: A category has many 4D wallpapers
     public function fourDwallpapers()
     {
         return $this->hasMany(FourDwallpaper::class, 'cat_id');
     }
-
-    // Relation to LiveWallpapers: A category has many live wallpapers
     public function liveWallpapers()
     {
         return $this->hasMany(LiveWallpapers_Panel::class, 'cat_id');
     }
 
-    // Relation to Events: A category can belong to many events
     public function events()
     {
         return $this->belongsToMany(Event::class, 'category_event', 'cat_id', 'event_id')
